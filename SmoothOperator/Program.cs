62fbec2c9c0f2ac34be1762d68c131816ae61a77
Cátia Nascimento <catia.nascimento2006@gmail.com>
2025-02-27 10:03:48 +0000

    Added incremento e decremento

--- SmoothOperator/Program.cs
+++ SmoothOperator/Program.cs
@@ -6,7 +6,11 @@ namespace SmoothOperator
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            Console.WriteLine("Please write an int number:");
+            string number = Console.ReadLine();
+            float savednumber = Parse(number)
+            number = number ++
+            
         }
     }
 }
