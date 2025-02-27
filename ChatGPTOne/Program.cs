663bacdb9aab942e0833f1ca62814cb22694de29
Cátia Nascimento <catia.nascimento2006@gmail.com>
2025-02-27 18:08:14 +0000

    Still trying to add exit but with if and else if

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net;
 
 namespace ChatGPTOne
 {
@@ -7,32 +8,38 @@ namespace ChatGPTOne
         private static void Main(string[] args)
         {
             // Colocar questão ao utilizador
-            Console.Write("Place your question? ");
+            while (question != "Exit")
             question = Console.ReadLine();
 
             // Verificar se se trata de uma questão conhecida e determinar uma
             // resposta adequada
-            switch (question)
+            Console.Write("Place your question? ");
             {
-                if "Who Coded you?":
+                if (question == "Who Coded you?")
+                {
                     response = "Catiamn";
-                    break;
-                case "Where are you?":
+                    continue;
+                }
+                else if (question == "Where are you?")
+                {
                     response = "At the center of the universe, at a point where everything is equally close by and everything is equally far away.";
-                    break;
-                case "What's your favorite color?":
+                    continue;
+                }
+                else if (question == "What's your favorite color?")
+                {
                     response = "Turquoise, not cyan!";
-                    break;
-                case "How are you?":
+                    continue;
+                }
+                else if (question == "How are you?")
+                {
                     response = "A little bored of all these questions";
-                    break;
-                default:
-                    response = "My programmer hasn't coded and answer for that yet";
-                    break;
-                case "EXIT":
-                    break
+                    continue;
+                }
+            else if (question != "EXIT")
+            {
+                response = "My programmer doesn't have an answer for that yet";
+                continue;
             }
-
             // Responder adequadamente
             Console.WriteLine(response);
         }
