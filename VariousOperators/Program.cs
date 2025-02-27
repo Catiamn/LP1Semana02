9bf8d874c0201f05316620ef285f55a50c6d4ec1
Cátia Nascimento <catia.nascimento2006@gmail.com>
2025-02-27 14:38:02 +0000

    Added ask for user input

---
+++ VariousOperators/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace VariousOperators
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insere número inteiro-não negativo:");
+            string number = Console.ReadLine();
+            byte saved_number = byte.Parse(number);
+            Console.WriteLine($"{saved_number/2}");
+            Console.WriteLine($"{saved_number<<3}");
+            Console.WriteLine($"{saved_number^6}");
+            Console.WriteLine($"{saved_number>10}");
+        }
+    }
+}
