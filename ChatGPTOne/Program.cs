3a46129f9ae507d73deedea1b24a23fcd5c29833
Cátia Nascimento <catia.nascimento2006@gmail.com>
2025-02-27 18:26:46 +0000

    Fixed some errors´

--- ChatGPTOne/Program.cs
+++ ChatGPTOne/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Net;
 
 namespace ChatGPTOne
 {
@@ -7,41 +6,46 @@ namespace ChatGPTOne
     {
         private static void Main(string[] args)
         {
+            string question, response;
+            question = "";
+            response = "My programmer doesn't have an answer for that yet";
             // Colocar questão ao utilizador
-            while (question != "Exit")
-            question = Console.ReadLine();
-
+            while (question != "EXIT")
+            {
+                Console.Write("Place your question? ");
+                question = Console.ReadLine();
             // Verificar se se trata de uma questão conhecida e determinar uma
             // resposta adequada
-            Console.Write("Place your question? ");
-            {
-                if (question == "Who Coded you?")
                 {
-                    response = "Catiamn";
-                    continue;
+                    if (question == "Who Coded you?")
+                    {
+                        response = "Catiamn";
+                
+                    }
+                    else if (question == "Where are you?")
+                    {
+                        response = "At the center of the universe, at a point where everything is equally close by and everything is equally far away.";
+                    
+                    }
+                    else if (question == "What's your favorite color?")
+                    {
+                        response = "Turquoise, not cyan!";
+                    
+                    }
+                    else if (question == "How are you?")
+                    {
+                        response = "A little bored of all these questions";
+                    
+                    }
+                    else if (question == "EXIT")
+                    {
+                        response = "";
+                    }
+                    // Responder adequadamente
+                Console.WriteLine(response);
                 }
-                else if (question == "Where are you?")
-                {
-                    response = "At the center of the universe, at a point where everything is equally close by and everything is equally far away.";
-                    continue;
-                }
-                else if (question == "What's your favorite color?")
-                {
-                    response = "Turquoise, not cyan!";
-                    continue;
-                }
-                else if (question == "How are you?")
-                {
-                    response = "A little bored of all these questions";
-                    continue;
-                }
-            else if (question != "EXIT")
-            {
-                response = "My programmer doesn't have an answer for that yet";
-                continue;
             }
-            // Responder adequadamente
-            Console.WriteLine(response);
         }
     }
 }
+
