42f794863cea8a1933e0bcba81607fb1f087653f
Cátia Nascimento <catia.nascimento2006@gmail.com>
2025-02-27 16:59:20 +0000

    Add Min and Max of every type

--- ValoresEspeciais/Program.cs
+++ ValoresEspeciais/Program.cs
@@ -6,7 +6,29 @@ namespace ValoresEspeciais
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            sbyte sbMax = sbyte.MaxValue;
+            sbyte sbMin = sbyte.MinValue;
+
+            byte bMax = byte.MaxValue;
+            byte bMin = byte.MinValue;
+
+            double dbMin = double.MinValue;
+            double dbMax = double.MaxValue;
+
+            float fMax = float.MaxValue;
+            float fMin = float.MinValue;
+
+            ulong ulMax = ulong.MaxValue;
+            ulong ulMin = ulong.MinValue;
+
+            long lMax = long.MaxValue;
+            long lMin = long.MinValue;
+
+            int iMax = int.MaxValue;
+            int iMin = int.MinValue;
+
+            uint uiMax = uint.MaxValue;
+            uint uiMin = uint.MinValue;
         }
     }
 }
