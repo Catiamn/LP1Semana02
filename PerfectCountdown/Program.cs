5de12bd32ed44949d1cb3f8322b45e5782780e25
Cátia Nascimento <catia.nascimento2006@gmail.com>
2025-02-28 14:23:51 +0000

    Added a while so its becomes a loop

--- PerfectCountdown/Program.cs
+++ PerfectCountdown/Program.cs
@@ -6,7 +6,20 @@ namespace PerfectCountdown
     {
         private static void Main(string[] args)
         {
-            Console.WriteLine("Hello LP1!");
+            while (true)
+            {
+                //Ask user for a Starting number for the countdown
+                Console.WriteLine("Start number:");
+                int start = int.Parse(Console.ReadLine());
+                //Ask user for a step number for countdown
+                Console.WriteLine("Step number:");
+                int step = int.Parse(Console.ReadLine());
+
+                if (step > start)
+                {
+                    Console.WriteLine("Out-of-range start or step. Try again.");
+                }
+            }
         }
     }
 }
